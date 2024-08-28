292b2b63a304fa3119d773b15aa84c9ae759560a
Mdn753 <yassinemdnpro@gmail.com>
2024-08-28 13:22:02 +0100

    commit

---
+++ src/main/java/com/DPETL/DPETL/repositories/MarcheDocumentsRepository.java
@@ -0,0 +1,7 @@
+package com.DPETL.DPETL.repositories;
+
+import com.DPETL.DPETL.models.MarcheDocuments;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarcheDocumentsRepository extends JpaRepository<MarcheDocuments,Integer> {
+}
---
+++ src/main/java/com/DPETL/DPETL/repositories/MarcheRepository.java
@@ -0,0 +1,7 @@
+package com.DPETL.DPETL.repositories;
+
+import com.DPETL.DPETL.models.Marche;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarcheRepository extends JpaRepository<Marche,Integer> {
+}
